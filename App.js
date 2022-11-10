8fb58113e9b027b4a854030c594506f3ada67011
Earnest1914 <112690649+Earnest1914@users.noreply.github.com>
2022-11-09 19:13:40 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <p className="App">
+        <img src={`${process.env.PUBLIC_URL}Fichas.png`} />
+        </p>
+        <p className="App">
+        <img src={`${process.env.PUBLIC_URL}Bitacora.png`} />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
